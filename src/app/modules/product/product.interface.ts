b45291c9924933efdc32231737231692287ecd65
Tanvir Hasan <57188940+Tanvir-art@users.noreply.github.com>
2024-05-22 18:11:20 +0600

    create interface and model

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,17 @@
+type variants = {
+  type: string;
+  value: string;
+};
+type inventory = {
+  quantity: number;
+  inStock: boolean;
+};
+export type Product = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: variants[];
+  inventory: inventory;
+};
---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,14 @@
+import { Schema, model, connect } from 'mongoose';
+import { Product } from './product.interface';
+
+const productShema = new Schema<Product>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [Object], required: true },
+  inventory: { type: Object, required: true },
+});
+
+const productModel = model<Product>('Product', productShema);
