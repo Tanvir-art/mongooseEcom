ec6435389a14e908b602253db4032e9937b97845
Tanvir Hasan <57188940+Tanvir-art@users.noreply.github.com>
2024-05-22 16:21:50 +0600

    setup the project

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    ignores: ["**/node_modules/**", ".dist/"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "off",
+      "no-console": "warn",
+      "prefer-const": "error",
+    },
+    //  globals: "readonly",
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
